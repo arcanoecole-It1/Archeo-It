24a574aa31e5277bc5c090f50c91ab011d918ace
Grace tchibinda <tchibindagrace5@gmail.com>
2025-05-26 14:38:35 +0100

    Ajout de style de la page de contact

--- contact.php
+++ contact.php
@@ -25,46 +25,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Archeo - It Contact</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./assets/css/header.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/footer.css">
+    <link rel="stylesheet" href="./assets/css/contact.css">
+  
+</head>
+<body>
+    <div class="container mt-5">
+        <h2>Contact</h2>
 
-<div class="container mt-5">
-    <h2>Contact</h2>
+        <?php if ($success): ?>
+            <div class="alert alert-success"><?= $success ?></div>
+        <?php endif; ?>
 
-    <?php if ($success): ?>
-        <div class="alert alert-success"><?= $success ?></div>
-    <?php endif; ?>
+        <?php if ($error): ?>
+            <div class="alert alert-danger"><?= $error ?></div>
+        <?php endif; ?>
 
-    <?php if ($error): ?>
-        <div class="alert alert-danger"><?= $error ?></div>
-    <?php endif; ?>
-
-    <form method="POST" action="contact.php">
-        <div class="mb-3">
-            <label>Nom :</label>
-            <input type="text" name="last_name" class="form-control" required>
-        </div>
-        <div class="mb-3">
-            <label>Prénom :</label>
-            <input type="text" name="first_name" class="form-control" required>
-        </div>
-        <div class="mb-3">
-            <label>Email :</label>
-            <input type="email" name="email" class="form-control" required>
-        </div>
-        <div class="mb-3">
-            <label>Sujet :</label>
-            <select name="subject" class="form-control" required>
-                <option value="">-- Choisissez un sujet --</option>
-                <option value="Demande d’infos">Demande d’infos</option>
-                <option value="Demande de Rendez-vous">Demande de Rendez-vous</option>
-                <option value="Autre">Autre</option>
-            </select>
-        </div>
-        <div class="mb-3">
-            <label>Message :</label>
-            <textarea name="comment" class="form-control" rows="4" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Envoyer</button>
-    </form>
-</div>
+        <form method="POST" action="contact.php">
+            <div class="mb-3">
+                <label>Nom :</label>
+                <input type="text" name="last_name" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label>Prénom :</label>
+                <input type="text" name="first_name" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label>Email :</label>
+                <input type="email" name="email" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label>Sujet :</label>
+                <select name="subject" class="form-control" required>
+                    <option value="">-- Choisissez un sujet --</option>
+                    <option value="Demande d’infos">Demande d’infos</option>
+                    <option value="Demande de Rendez-vous">Demande de Rendez-vous</option>
+                    <option value="Déclarer bug ">Déclarer bug</option>
+                    <option value="Demande d'emploi">Demande d'emploi</option>
+                    <option value="Autre">Autre</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label>Message :</label>
+                <textarea name="comment" class="form-control" rows="4" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
+    </div>
+    </body>
+</html>
+ <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
 <?php include 'footer.php'; ?>
