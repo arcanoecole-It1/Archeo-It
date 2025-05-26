20b16da571680a859fc5023148ef483b9d3a37d7
Grace tchibinda <tchibindagrace5@gmail.com>
2025-05-26 17:52:16 +0100

    a	szjhj,

--- footer.php
+++ footer.php
@@ -11,7 +11,6 @@
         <div class="dist">
             <h4>À propos de Archeo -It</h4>
             <p style="font-size: 1.2em;">
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
                 Archeo-It est un site dédié à l'archéologie, offrant des ressources, des articles et des outils pour les passionnés et les professionnels du domaine. Notre mission est de promouvoir la connaissance et la préservation du patrimoine archéologique à travers le monde.
             </p><br>
         </div>
@@ -21,14 +20,8 @@
             <p>&copy; <span>Archeo-It</span> 2025. Tous droits réservés.</p>
         </div>
         <div class="div-link">
-          <a href="https://www.facebook.com" target="_blank">
-    <i class="fa fa-facebook"></i>
-</a>
-
-<a href="https://github.com" target="_blank">
-    <i class="fa fa-github"></i>
-</a>
-
+            <a class="foot-link" target="_blanc" href="https://www.facebook.com/profile.php?id=61560523441372"><img src="assets/images/face.png" alt="Facebook"></a>
+            <a class="foot-link" target="_blanc" href="https://github.com/arcanoecole-It1/workspace"><img src="assets/images/git2.png" alt="Github"></a>
         </div>
     </div>
 </footer>
