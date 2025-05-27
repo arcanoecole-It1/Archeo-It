d6bfc74e8caab8b949a3204a4fd4fd54bb88a832
Grace tchibinda <tchibindagrace5@gmail.com>
2025-05-27 08:47:35 +0100

    jn,

--- footer.php
+++ footer.php
@@ -20,17 +20,8 @@
             <p>&copy; <span>Archeo-It</span> 2025. Tous droits réservés.</p>
         </div>
         <div class="div-link">
-<<<<<<< HEAD
             <a class="foot-link" target="_blanc" href="https://www.facebook.com/profile.php?id=61560523441372"><img src="assets/images/face.png" alt="Facebook"></a>
             <a class="foot-link" target="_blanc" href="https://github.com/arcanoecole-It1/workspace"><img src="assets/images/git2.png" alt="Github"></a>
-=======
-            <a class="foot-link" target="_blanc" href="https://www.facebook.com/profile.php?id=61560523441372">
-                <ion-icon class="logo-facebook" name="logo-facebook"></ion-icon>
-            </a>
-            <a class="foot-link" target="_blanc" href="https://github.com/arcanoecole-It1/workspace">
-                <ion-icon class="logo-github" name="logo-github"></ion-icon>
-            </a>
->>>>>>> f67029e2cdbda0bf2b9d0cf6feae0813d028fee9
         </div>
     </div>
 </footer>
