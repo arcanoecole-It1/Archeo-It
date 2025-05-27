a504d0f3f658a0397bc5935f4e380ea37b36da70
Cedric Richard <109342@ecole-it.com>
2025-05-27 09:00:35 +0200

    Nouvelle couleur

--- creation.php
+++ creation.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Creation - Admins</title>
+    <title>Creation Actualités, Chantier , Admins, - Admins</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./assets/css/header.css">
@@ -12,7 +12,25 @@
 </head>
 <body>
     <?php require 'header.php'; ?>
-
+    <main>
+        <div>
+            <h2>
+                Bienvenue sur la page de création des actualités, chantiers et admins
+            </h2>
+            <p>
+                Cette page est réservée aux administrateurs du site. Vous pouvez créer, modifier ou supprimer des actualités, des chantiers et des comptes administrateurs.
+            </p>
+            <a href="admin_create.php">
+                <div></div>
+            </a>
+            <a href="actualite_create.php">
+                <div></div>
+            </a>
+            <a href="chantier_create.php">
+                <div></div>
+            </a>
+        </div>
+    </main>
     <?php
     require 'footer.php';
     ?>
