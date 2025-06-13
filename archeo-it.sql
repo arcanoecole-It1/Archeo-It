b9c25f2fa71804c64edb15e183c46c8619f0b8db
Cedric Richard <109342@ecole-it.com>
2025-06-13 20:05:09 +0200

    nouvelle base de donnée

--- archeo-it.sql
+++ archeo-it.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3307
--- Généré le : lun. 05 mai 2025 à 08:37
+-- Généré le : ven. 13 juin 2025 à 18:04
 -- Version du serveur : 11.5.2-MariaDB
 -- Version de PHP : 8.3.14
 
@@ -31,42 +31,95 @@ DROP TABLE IF EXISTS `actualites`;
 CREATE TABLE IF NOT EXISTS `actualites` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `titre` varchar(255) NOT NULL,
-  `contenu` text NOT NULL,
-  `date_creation` date NOT NULL,
-  `id_createur` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_createur` (`id_createur`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+  `description` text NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `date_creation` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Déchargement des données de la table `actualites`
+--
+
+INSERT INTO `actualites` (`id`, `titre`, `description`, `image`, `date_creation`) VALUES
+(10, 'testdtjutev', '&lt;egdvhjyfy', 'uploads/actualites/684c2bb7e2577.webp', 1749822391);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `chantier`
+-- Structure de la table `agenda`
 --
 
-DROP TABLE IF EXISTS `chantier`;
-CREATE TABLE IF NOT EXISTS `chantier` (
+DROP TABLE IF EXISTS `agenda`;
+CREATE TABLE IF NOT EXISTS `agenda` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `titre` varchar(255) NOT NULL,
-  `contenu` text NOT NULL,
-  `id_createur` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `date_evenement` date NOT NULL,
+  `heure_evenement` time DEFAULT NULL,
+  `lieu` varchar(255) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `id_createur` (`id_createur`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+  KEY `created_by` (`created_by`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Déchargement des données de la table `agenda`
+--
+
+INSERT INTO `agenda` (`id`, `titre`, `description`, `date_evenement`, `heure_evenement`, `lieu`, `created_by`, `created_at`) VALUES
+(1, 'testdtjut', 'zehsjnyegvtbtdy', '2025-06-26', '20:59:00', 'afererghb', 1, '2025-06-13 15:57:02');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `commentaire`
+-- Structure de la table `chantiers`
 --
 
-DROP TABLE IF EXISTS `commentaire`;
-CREATE TABLE IF NOT EXISTS `commentaire` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `comment` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+DROP TABLE IF EXISTS `chantiers`;
+CREATE TABLE IF NOT EXISTS `chantiers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(500) DEFAULT NULL,
+  `localisation` varchar(255) NOT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date DEFAULT NULL,
+  `statut` enum('planifie','actif','termine') DEFAULT 'actif',
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Déchargement des données de la table `chantiers`
+--
+
+INSERT INTO `chantiers` (`id`, `nom`, `description`, `image`, `localisation`, `date_debut`, `date_fin`, `statut`, `created_at`) VALUES
+(5, 'nicolas', 'z&#039;hz', 'uploads/chantiers/684c2b846844b.jpg', 'h', '2025-05-31', '2025-06-19', 'actif', '2025-06-13 13:45:40');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaires`
+--
+
+DROP TABLE IF EXISTS `commentaires`;
+CREATE TABLE IF NOT EXISTS `commentaires` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `commentaire` text NOT NULL,
+  `date_creation` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Déchargement des données de la table `commentaires`
+--
+
+INSERT INTO `commentaires` (`id`, `nom`, `email`, `commentaire`, `date_creation`) VALUES
+(1, 'Richard', 'test@gmail.com', 'bçpfp_', '2025-06-13 18:43:08');
 
 -- --------------------------------------------------------
 
@@ -83,7 +136,14 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `subject` varchar(255) NOT NULL,
   `comment` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id`, `last_name`, `first_name`, `email`, `subject`, `comment`) VALUES
+(2, 'Bintcha', 'Cedric', 'test@gmail.com', 'Demande de Rendez-vous', 'wze(tyil');
 
 -- --------------------------------------------------------
 
@@ -94,14 +154,24 @@ CREATE TABLE IF NOT EXISTS `contact` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
   `is_admin` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `is_admin`) VALUES
+(3, 'Kadi', 'testings@test.fr', '$2y$10$OJMPxhc2OyMHxDielwvcGOXepbCkPRbYfrUiwdKz0VnkN/3/ohlwe', 'Grace', 'Kadidia', 1),
+(2, 'Richard', 'useless@email.com', '$2y$10$GKb2s9uZAi.yRiIoyWZ1Be1.WrKreTd2P2UjNAQBipP.XO3UISBui', 'test', 'test', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
